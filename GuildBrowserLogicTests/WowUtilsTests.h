dbc374ae7b8a855c09b85f0f8b057e47a015ad9a
Robert Lawson <rlawson@grade-eight.co>
2014-02-08 08:04:46 -0800

    This is our commit and push done while forgetting to test before hand, to see what Jenkins finds.

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,18 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Robert Lawson on 2/7/14.
+//  Copyright (c) 2014 Charlie Fulton. All rights reserved.
+//
+
+#ifndef GuildBrowser_WowUtilsTests_h
+#define GuildBrowser_WowUtilsTests_h
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
+
+
+#endif
---
+++ GuildBrowserLogicTests/WowUtilsTests.m
@@ -0,0 +1,51 @@
+//
+//  WowTestUtils.m
+//  GuildBrowser
+//
+//  Created by Robert Lawson on 2/7/14.
+//  Copyright (c) 2014 Charlie Fulton. All rights reserved.
+//
+
+//#import "WowUtilsTests.h"
+#import "WowUtils.h"
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
+
+@implementation WowUtilsTests
+
+// 1
+-(void)testCharacterClassNameLookup
+{
+    // 2
+    STAssertEqualObjects(@"Warrior",
+                         [WoWUtils classFromCharacterType:1],
+                         @"ClassType should be Warrior");
+    // 3
+    STAssertFalse([@"Mage" isEqualToString:[WoWUtils classFromCharacterType:2]],
+                  nil);
+	
+    // 4
+    STAssertTrue([@"Paladin" isEqualToString:[WoWUtils classFromCharacterType:2]],
+                 nil);
+    // add the rest as an exercise
+}
+
+- (void)testRaceTypeLookup
+{
+    STAssertEqualObjects(@"Human", [WoWUtils raceFromRaceType:1], nil);
+    STAssertEqualObjects(@"Orc", [WoWUtils raceFromRaceType:2], nil);
+    STAssertFalse([@"Night Elf" isEqualToString:[WoWUtils raceFromRaceType:45]],nil);
+    // add the rest as an exercise
+}
+
+- (void)testQualityLookup
+{
+    STAssertEquals(@"Grey", [WoWUtils qualityFromQualityType:1], nil);
+    STAssertFalse([@"Purple" isEqualToString:[WoWUtils qualityFromQualityType:10]],nil);
+    // add the rest as an exercise
+}
+
+@end
